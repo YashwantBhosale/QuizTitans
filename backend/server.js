f6b5b081aadff2ba80206eeca55cce42848a32de
shweta-585 <sweta.app19@gmail.com>
2024-12-06 23:06:05 +0530

    Debug backend vercel.json

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import 'dotenv/config'
 import  mongoose from "mongoose"
 import cors from "cors"
-import QuizSchema from "./models/quizSchema.js"
 import QuizData from "./models/quizSchema.js"
 
 const PORT = process.env.PORT || 4000;
@@ -14,16 +13,12 @@ app.use(cors());
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Middleware to parse URL-encoded data (for form submissions)
-// app.use(express.urlencoded({ extended: true }));
-
 // CONNECT MONGOOSE
 const uri = process.env.MONGO_URI;
 mongoose
     .connect(uri)
     .then(() => console.log("MongoDB connected!!"))
     .catch(err => console.log("Error connecting", err));
-
 
 app.get("/", (req, res) => {
     res.send("hello");
