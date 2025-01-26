3a1cf948823d11aaa2747772bfe09d9f246ddceb
shweta-585 <sweta.app19@gmail.com>
2025-01-26 14:31:50 +0530

    Created carousel using swiper

--- frontend/src/Pages/ShowQuiz.jsx
+++ frontend/src/Pages/ShowQuiz.jsx
@@ -2,6 +2,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "../styles/showquiz.module.css";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const ShowQuiz = () => {
   const [QuizData, SetQuizData] = useState([]);
@@ -14,42 +19,52 @@ const ShowQuiz = () => {
   ];
   const API = "http://localhost:4000/quiz/all";
 
-  // Use Effect Hook
+  // Fetch Quiz Data
   useEffect(() => {
     axios
       .get(API)
       .then((res) => {
+        console.log(res.data);
         SetQuizData(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []); // [] empty list no dependancy
-
-  let carouselDiv = document.querySelector(`${styles.quiz_domain_list}`);
-  console.log(carouselDiv);
+  }, []);
 
   return (
-    <>
+    <div className={styles.view_quiz_main}>
       {DomainData.map((dom) => (
-        <div className={styles.QuizList} key={dom}>
-        <h1 className={styles.domainName}>{dom}</h1>
-          <div id={styles[dom]} className={styles.quiz_domain_list}>
-            {QuizData.filter((d) => d.domain == dom).map((d) => (
-              <div key={d._id} className={styles.quiz_box}>
-                <p>{d.title}</p>
-                <p>{d.domain}</p>
+        <div className={styles.quiz_container} key={dom}>
+          <h1 className={styles.domainName}>{dom}</h1>
+
+          <Swiper
+            modules={[Navigation, Pagination]}
+            loop={true}
+            spaceBetween={30}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              768: { slidesPerView: 2 },
+              1024: { slidesPerView: 4 },
+            }}
+            className={styles.swiper_container}
+          >
+            {QuizData.filter((d) => d.domain === dom).map((d) => (
+              <SwiperSlide key={d._id} className={styles.quiz_item}>
+                <h2>{d.title}</h2>
+                <h4>{d.domain}</h4>
                 <p>{d.describe}</p>
                 <Link className={styles.quiz_btn} to={`/quiz/take/${d._id}`}>
-                Take Quiz
+                  Take Quiz
                 </Link>
-              </div>
+              </SwiperSlide>
             ))}
-            {/* <div className={styles. }></div> */}
-          </div>
+          </Swiper>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
